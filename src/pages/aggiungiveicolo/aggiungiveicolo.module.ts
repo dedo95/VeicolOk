a2013ee457657a90a1bcfb4d5e276fff24b5e06d
Antonio21061995 <34509909+Antonio21061995@users.noreply.github.com>
2018-10-04 15:29:09 +0200

    pagina aggiungiveicolo

---
+++ src/pages/aggiungiveicolo/aggiungiveicolo.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AggiungiveicoloPage } from './aggiungiveicolo';
+
+@NgModule({
+  declarations: [
+    AggiungiveicoloPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AggiungiveicoloPage),
+  ],
+})
+export class AggiungiveicoloPageModule {}
