0aa3fb030e03c9c7ddb2abe9fbef296ca6fe9ae3
daniemeo <37437362+daniemeo@users.noreply.github.com>
2019-01-05 18:23:15 +0100

    inserimento model

---
+++ src/model/patente.model.ts
@@ -0,0 +1,8 @@
+export class Patente{
+    num_patente: String;
+    intestatario: String;
+    punti: BigInteger;
+    categoria: String;
+    data_scadenza: Date;
+   
+}
---
+++ src/model/veicolo.model.ts
@@ -0,0 +1,12 @@
+export class Veicolo{
+    targa: String;
+    desc: String;
+    tipologia: String;
+    colore: String;
+    anno_imm: Date;
+    alimentazione: String;
+    cilindrata: bigint;
+    kilowatt: bigint;
+    cavalli_motore: bigint;
+    img_veicol: string;
+}
